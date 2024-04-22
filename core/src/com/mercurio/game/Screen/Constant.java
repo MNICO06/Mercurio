2ac6125a7b7802e36c7841d0eedeb1b31f3448db
Nicolò Molteni <molteni.nico06@gmail.com>
2024-04-22 16:10:54 +0200

    crezione classe full map

--- core/src/com/mercurio/game/Screen/Constant.java
+++ core/src/com/mercurio/game/Screen/Constant.java
@@ -6,4 +6,6 @@ public class Constant {
     public static final String CASA_ASH_SCREEN = "CasaSpawn";
     public static final String CENTRO_POKEMON = "mappa/CentroPokemon.tmx";
     public static final String CENTRO_POKEMON_SCREEN = "centroPokemon";
+    public static final String MAPPA_SCREEN = "mappa";
+    public static final String MAPPA = "mappa/FullMapLimited.tmx";
 }
---
+++ core/src/com/mercurio/game/Screen/FullMap.java
@@ -0,0 +1,55 @@
+package com.mercurio.game.Screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+
+public class FullMap extends ScreenAdapter{
+    private final MercurioMain game;
+    
+    private TiledMap mappa;
+    private OrthogonalTiledMapRenderer tileRenderer;
+    private OrthographicCamera camera;
+    private Vector2 map_size;
+    private MapLayer lineeLayer;
+
+    public FullMap(MercurioMain game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        TmxMapLoader mapLoader = new TmxMapLoader();
+        mappa = mapLoader.load(Constant.MAPPA);
+        tileRenderer = new OrthogonalTiledMapRenderer(mappa);
+
+        //calcolo e assegno dimensioni alla mappa
+        int mapWidth = mappa.getProperties().get("width", Integer.class) * mappa.getProperties().get("tilewidth", Integer.class);
+        int mapHeight = mappa.getProperties().get("height", Integer.class) * mappa.getProperties().get("tileheight", Integer.class);
+        map_size = new Vector2(mapWidth,mapHeight);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, map_size.x/1.9f, map_size.y/2f);
+        camera.update();
+
+        game.setMap(mappa, tileRenderer, camera, map_size.x, map_size.y);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        lineeLayer = game.getLineeLayer();
+        cambiaProfondita(lineeLayer);
+    }
+
+    private void cambiaProfondita(MapLayer lineeLayer) {
+        
+    }
+}
