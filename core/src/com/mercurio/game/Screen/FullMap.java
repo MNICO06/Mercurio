d53244f07a1c2a2c66c2342449d8591c9d1bbc24
MNICO06 <82870159+MNICO06@users.noreply.github.com>
2024-04-25 22:43:59 +0200

    Update FullMap.java

--- core/src/com/mercurio/game/Screen/FullMap.java
+++ core/src/com/mercurio/game/Screen/FullMap.java
@@ -7,10 +7,7 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.MapLayer;
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -24,14 +21,14 @@ public class FullMap extends ScreenAdapter{
     private Vector2 map_size;
     private MapLayer lineeLayer;
 
-    public FullMap(MercurioMain game) {
+    public FullMap(MercurioMain game, TiledMap mappa) {
         this.game = game;
+        this.mappa = mappa;
     }
 
+    
     @Override
     public void show() {
-        TmxMapLoader mapLoader = new TmxMapLoader();
-        mappa = mapLoader.load(Constant.MAPPA);
         tileRenderer = new OrthogonalTiledMapRenderer(mappa);
 
         //calcolo e assegno dimensioni alla mappa
@@ -58,57 +55,4 @@ public class FullMap extends ScreenAdapter{
         ArrayList<String> background = new ArrayList<String>();
         ArrayList<String> foreground = new ArrayList<String>();
 
-        background.add("Livello tile ground");
-        background.add("Livello tile case");
-        background.add("Livello tile deco2");
-        background.add("Livello tile deco1");
-        background.add("Livello tile deco");
-        background.add("Livello tile path");
-        background.add("Livello tile piantine");
-        background.add("AlberiMezzo");
-        background.add("AlberiFondo");
-        background.add("FixingLayer1");
-
-        foreground.add("AlberiCima");
-
-        
-        for (MapObject object : lineeLayer.getObjects()) {
-            if (object instanceof RectangleMapObject) {
-                RectangleMapObject rectangleObject = (RectangleMapObject)object;
-
-                //salvo il nome del layer che verrà inserito in una delle due liste
-                String layerName = (String)rectangleObject.getProperties().get("layer");
-
-                if (game.getPlayer().getPlayerPosition().y < rectangleObject.getRectangle().getY()) {
-                    background.add(layerName);
-                }else {
-                    foreground.add(layerName);
-                }
-            }
-        }
-
-        //background
-        for (String layerName : background) {
-            renderLayer(layerName);
-        }
-
-        game.renderPlayer();
-
-        //foreground
-        for (String layerName : foreground) {
-            renderLayer(layerName);
-        }
-
-    }
-
-    private void renderLayer(String layerName) {
-        tileRenderer.getBatch().begin();
-        
-        // Recupera il layer dalla mappa
-        MapLayer layer = mappa.getLayers().get(layerName);
-        // Renderizza il layer
-        tileRenderer.renderTileLayer((TiledMapTileLayer)layer);
-
-        tileRenderer.getBatch().end();
-    }
-}
+        tileRenderer.render();
