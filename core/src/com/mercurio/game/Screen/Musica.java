caa1e080594721fd6dd44e7edb29e22e024c6225
Nicolò Molteni <molteni.nico06@gmail.com>
2024-04-28 22:59:24 +0200

    aggiunta musica in start, p1, bosco e capitale

--- core/src/com/mercurio/game/Screen/Musica.java
+++ core/src/com/mercurio/game/Screen/Musica.java
@@ -6,9 +6,12 @@ import com.badlogic.gdx.audio.Music;
 public class Musica {
     AssetManager assetManager;
     private Music currentMusic;
+    MercurioMain game;
+    private String current;
 
-    public Musica(AssetManager assetManager) {
+    public Musica(MercurioMain game, AssetManager assetManager) {
         this.assetManager = assetManager;
+        this.game = game;
     }
     
     public void playMusic(String musicPath) {
@@ -29,6 +32,41 @@ public class Musica {
         // Interrompi la riproduzione della musica se è attualmente in corso
         if (currentMusic != null && currentMusic.isPlaying()) {
             currentMusic.stop();
+        }
+    }
+
+    public void startMusic(String luogo) {
+        switch (luogo) {
+            case "start":
+                if (current != "start") {
+                    playMusic("musica/start.mp3");
+                    current = "start";
+                }
+                break;
+            
+            case "percorso1":
+                if (current != "percorso1") {
+                    playMusic("musica/firstPercorso.mp3");
+                    current = "percorso1";
+                }
+                break;
+
+            case "bosco":
+                if (current != "bosco") {
+                    playMusic("musica/bosco.mp3");
+                    current = "bosco";
+                }
+                break;
+
+            case "capitale":
+                if (current != "capitale") {
+                    playMusic("musica/canzoneCheSpacca.mp3");
+                    current = "capitale";
+                }
+                break;
+
+            default:
+                break;
         }
     }
 
