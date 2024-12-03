bbec5d41e8ad3810003803c9852c5ce4936ef14e
LupoAlberto69 <moltenilorenzo6@gmail.com>
2024-12-03 17:16:00 +0100

    Try On branch

---
+++ core/src/com/mercurio/game/AssetManager/GameAsset.java
@@ -0,0 +1,15 @@
+package com.mercurio.game.AssetManager;
+
+import com.badlogic.gdx.assets.AssetManager;
+
+public class GameAsset{
+    private static final AssetManager asset = new AssetManager();
+
+    public static AssetManager getAsset() {
+        return asset;
+    }
+
+    public static void loadBattlaAssets(){
+        
+    }
+}
