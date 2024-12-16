3500f64a808273510533a04476f3fed4593a3697
P1zz4_M4n <158455102+P1zz4M4n@users.noreply.github.com>
2024-12-16 09:29:30 +0100

    Update MiniMappa.java

--- core/src/com/mercurio/game/strumenti/MiniMappa.java
+++ core/src/com/mercurio/game/strumenti/MiniMappa.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
 import com.mercurio.game.AssetManager.GameAsset;
+import com.mercurio.game.AssetManager.GameAsset.AssetMMappa;
 import com.mercurio.game.Screen.MercurioMain;
 import com.mercurio.game.menu.Borsa;
 
